c7590af8652da87e61870ae9f68f68aa6177c063
ronaldjeq9512 <ronaldjhairestelaquintana@gmail.com>
2024-04-20 23:26:47 -0500

    added icons and update logos

--- src/components/Header.component.tsx
+++ src/components/Header.component.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-/* import LogoIcon from './logo.svg';
- */
+import {Icon} from '@rneui/base';
+import LogoIcon from '../assets/icons/logo.svg';
 export function HeaderComponent() {
   return (
     <View>
-      <View>{/*  <LogoIcon /> */}</View>
       <View>
+        <LogoIcon />
+      </View>
+      <View>
+        <Icon type="material" name="phone" />
         <Text>(01) 411 6001</Text>
       </View>
     </View>
